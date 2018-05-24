31219a4cd21c87afe23209e8cb14cd80dd188520
Miguel Crespo <mcrespo@clickdelivery.com>
2018-05-23 19:21:39 -0500

    Basic cli

--- cli/index.js
+++ cli/index.js
@@ -4,11 +4,37 @@
  * Module dependencies.
  */
 
-var program = require('commander');
+const bluebird = require('bluebird');
+const fs = bluebird.promisifyAll(require('fs'));
+const program = require('commander');
+const glob = bluebird.promisify(require('glob'));
 
 program
   .command('report')
-  .parse(process.argv).action((type, args) => {
-    console.log('Type: ', type);
-    console.log('Args: ', args);
+  .description('This command will report data to your firebase backend')
+  .action(async () => {
+    try {
+      const config = JSON.parse(
+        fs.readFileSync('./web-reporter.json', 'utf-8')
+      );
+
+      if (!config.glob) {
+        console.error('Ups you have to specify the glob of your files');
+        return;
+      }
+
+      const filesToAnalyze = await glob(config.glob);
+
+      const stats = await bluebird.all(
+        filesToAnalyze.map(file => fs.statAsync(file))
+      );
+
+      console.log('Config: ', filesToAnalyze);
+      console.log('Stats: ', stats);
+      return 0;
+    } catch (e) {
+      console.error('There was an error: ', e);
+    }
   });
+
+program.parse(process.argv);
